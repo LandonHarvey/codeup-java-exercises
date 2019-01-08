8e2bcbb811c5cb61825ed49c2989e5ca00d66850
Landon Harvey <landonharvey5@gmail.com>
2019-01-08 10:08:38 -0600

    second piece

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,11 +2,27 @@ import java.sql.SQLOutput;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
+//        int i = 5;
+//        while (i < 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        long x = 2;
+//        do {
+//            System.out.println(i);
+//            x = x * x;
+//        }while (x < 1000000);
+
+        long x = 2;
+        for (int i = 5; i < 15; i++) {
             System.out.println(i);
-            i++;
+            if (x < 1000000) {
+                System.out.println(x);
+                x = x * x;
+            }
         }
     }
 }
+
 
