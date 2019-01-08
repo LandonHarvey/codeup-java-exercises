ae271e5347a73dc987c365a4e796774e77ff5099
Landon Harvey <landonharvey5@gmail.com>
2019-01-08 09:44:14 -0600

    first piece of loop excer

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
