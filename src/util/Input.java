cb1e1aee2ff13e117eab952fd4912c4f4b24e008
Landon Harvey <landonharvey5@gmail.com>
2019-01-22 10:45:47 -0600

    exceptions and error handeling

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Input {
@@ -44,55 +45,113 @@ public class Input {
 
     public static int getInt(int min, int max, String prompt) {
         System.out.println(prompt);
-        userInt = scanner.nextInt();
+        try {
+            userInt = scanner.nextInt();
+        } catch (InputMismatchException ex) {
+            System.out.println("User did not type Integer!");
+        }
         if (userInt > min && userInt < max) {
             return userInt;
-        } else {
+        } else if (userInt < min && userInt > max) {
             getInt(min, max);
+            return userInt;
+        }else {
+            System.out.println("Didn't type correct Number ");
             return userInt;
         }
     }
     public static int getInt(int min, int max) {
-        userInt = scanner.nextInt();
+        System.out.println("Type Input: ");
+        try {
+            userInt = scanner.nextInt();
+        } catch (InputMismatchException ex) {
+            System.out.println("User did not type Integer!");
+        }
+
         if (userInt > min && userInt < max) {
             return userInt;
-        } else {
+        } else if (userInt < min && userInt > max) {
             getInt(min, max);
+            return userInt;
+        }else {
+            System.out.println("Didn't type correct Number");
             return userInt;
         }
     }
 
     public static int getInt () {
         System.out.println("Enter Number");
-        userInt = scanner.nextInt();
+        try {
+            userInt = scanner.nextInt();
+        } catch (InputMismatchException ex) {
+            System.out.println("User did not type Integer!");
+        }
         return userInt;
     }
 
     public static double  getDouble(double min, double max, String prompt) {
         System.out.println(prompt);
         System.out.println("Number with decimal please");
-        userDouble = scanner.nextInt();
+        try {
+        userDouble = scanner.nextDouble();
+        } catch (InputMismatchException ex){
+            System.out.println("User did not type Number with decimals");
+        }
         if (userDouble > min && userDouble < max) {
             return userDouble;
-        } else {
+        } else if (userDouble < min && userDouble > max){
             getDouble(min, max);
+            return userDouble;
+        }else {
             return userDouble;
         }
     }
 
     public static double  getDouble(double min, double max) {
         System.out.println("Number with decimal please");
-        userDouble = scanner.nextInt();
+        try {
+            userDouble = scanner.nextDouble();
+        } catch (InputMismatchException ex){
+            System.out.println("User did not type Number with decimals");
+        }
         if (userDouble > min && userDouble < max) {
             return userDouble;
-        } else {
+        } else if (userDouble < min && userDouble > max){
             getDouble(min, max);
+            return userDouble;
+        }else {
+            System.out.println("Wrong");
             return userDouble;
         }
     }
 
     public static Double getDouble() {
+        System.out.println("Number with decimal please");
+        userDouble = scanner.nextDouble();
         return userDouble;
     }
 
+    public static Integer getBinary(String s) {
+        int numberConv = 0;
+        try {
+        numberConv = Integer.parseInt(s,2);
+        System.out.println(numberConv);
+        return numberConv;
+        } catch (NumberFormatException ex) {
+            System.out.println("Number not a number or String Number!");
+        }
+        return numberConv;
+    }
+
+    public static Integer getHex(String s) {
+        int numberConv = 0;
+        try{
+            numberConv = Integer.parseInt(s, 16);
+            System.out.println(numberConv);
+            return numberConv;
+        } catch (NumberFormatException ex) {
+            System.out.println("Number not a number or String!");
+        }
+        return numberConv;
+    }
 }
